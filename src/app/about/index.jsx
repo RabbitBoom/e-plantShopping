bce6ba1c08a99acf384fe9c447710899b1fba57f
rabbitBoom <276671662@qq.com>
2025-08-25 23:51:33 +0800

    feat: Modify header title to match repository name

--- src/app/about/index.jsx
+++ src/app/about/index.jsx
@@ -4,7 +4,7 @@ function AboutUs() {
   return (
     <div className="about-us-container">
       <h1 className="about-us-heading">About Us</h1>
-      <p className="about-us-description">Welcome to Shopping Center, where green meets serenity!</p>
+      <p className="about-us-description">Welcome to RabbitBoom's Demo, where green meets serenity!</p>
       <p className="about-us-content">
         Just a demo test!
       </p>
